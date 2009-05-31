5f816ad3d7a3448c7c5654b16f5ed0d8b1933ea9
Steve Jenson <stevej@pobox.com>
2009-05-30 19:49:47 -0700

    moving test_mailslot.rb to a better location.

--- src/scripts/test_mailslot.rb
+++ src/scripts/test_mailslot.rb
@@ -6,5 +6,5 @@ smtp = Net::SMTP.new('localhost', 10025)
 smtp.esmtp = false
 smtp.set_debug_output $stderr
 smtp.start("localhost") do |smtp|
-  smtp.send_message "hello!", 'your@mail.address', 'his_addess@example.com'
+  smtp.send_message "hello, work email address!", 'stevej@pobox.com', 'stevej@twitter.com'
 end
