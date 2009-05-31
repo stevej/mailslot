117fb490d5348001ee5e2b413036ed20e7b5c094
Steve Jenson <stevej@pobox.com>
2009-05-30 22:25:34 -0700

    tightening up the simple load test.

--- src/scripts/test_mailslot.rb
+++ src/scripts/test_mailslot.rb
@@ -4,7 +4,13 @@ require 'net/smtp'
 
 smtp = Net::SMTP.new('localhost', 10025)
 smtp.esmtp = false
-smtp.set_debug_output $stderr
-smtp.start("localhost") do |smtp|
-  smtp.send_message "hello, work email address!", 'stevej@pobox.com', 'stevej@twitter.com'
+#smtp.set_debug_output $stderr
+now = Time.now.to_i
+puts "sending 1000 emails"
+1000.times do
+  smtp.start("localhost") do |smtp|
+    smtp.send_message "From: stevej@pobox.com\n\nhello, work email address!", 'stevej@pobox.com', 'stevej@twitter.com'
+  end
 end
+completed = Time.now.to_i - now
+puts "Average milliseconds to send email: #{completed}"
